370c2bed33368a93c6a00a50a4df58324d2175e2
n00b <n00bskillz87@gmail.com>
2024-11-15 08:20:18 -0500

    Finalized support for emscripten build

--- rcbasic_runtime/rc_os_defines.h
+++ rcbasic_runtime/rc_os_defines.h
@@ -1,8 +1,8 @@
 #ifndef RC_OS_DEFINES_H_INCLUDED
 #define RC_OS_DEFINES_H_INCLUDED
 
-#define RC_LINUX
-//#define RC_WEB
+//#define RC_LINUX
+#define RC_WEB
 //#define RC_WINDOWS
 //#define RC_ANDROID
 //#define RC_MAC
