33ece2b755f18c89f9920bbeb7f93d99904ec595
n00b <n00bskillz87@gmail.com>
2024-11-19 00:04:17 -0500

    Emscripten build script

--- rcbasic_runtime/em_setup.sh
+++ rcbasic_runtime/em_setup.sh
@@ -40,35 +40,36 @@ export AN8_INCLUDE=/home/n00b/Projects/an8-parser
 export BOX2D_LIB=/home/n00b/Projects/box2d-2.4.2/box2d-2.4.2/em_build/bin
 export BOX2D_INCLUDE=/home/n00b/Projects/box2d-2.4.2/box2d-2.4.2/include
 
+export RCBASIC_SRC_PATH=$PWD/..
 
-em++ main.cpp \
-	theoraplay.c \
-	gui_freetype_font.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletBoxShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletBvhTriangleMeshShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCapsuleShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionCallBackInformation.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffectorAttract.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffector.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffectorDelete.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionObject.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCollisionShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletcommon.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletConeShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletConvexHullShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBullet.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletCylinderShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletGhostObject.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletGImpactMeshShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletLiquidBody.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletMotionState.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletPhysicsDebug.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletRayCastVehicle.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletRigidBody.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletSoftBody.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletSphereShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletTriangleMeshShape.cpp \
-	$IRR_BULLET_SRC_DIR/irrBulletWorld.cpp \
+emcc -c $RCBASIC_SRC_PATH/main.cpp \
+	-c $RCBASIC_SRC_PATH/theoraplay.c \
+	-c $RCBASIC_SRC_PATH/gui_freetype_font.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletBoxShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletBvhTriangleMeshShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCapsuleShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionCallBackInformation.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffectorAttract.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffector.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionObjectAffectorDelete.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionObject.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCollisionShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletcommon.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletConeShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletConvexHullShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBullet.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletCylinderShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletGhostObject.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletGImpactMeshShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletLiquidBody.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletMotionState.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletPhysicsDebug.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletRayCastVehicle.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletRigidBody.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletSoftBody.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletSphereShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletTriangleMeshShape.cpp \
+	-c $IRR_BULLET_SRC_DIR/irrBulletWorld.cpp \
 	"-I$IRR_BULLET_INCLUDE" \
 	"-L$THEORA_LIB" \
 	"-I$THEORA_INCLUDE" \
@@ -96,7 +97,7 @@ em++ main.cpp \
 	-lbox2d \
 	--use-port=freetype \
 	-s FULL_ES2=1 \
-	-s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s USE_SDL_NET=2 -s USE_OGG=1 -s USE_VORBIS=1 -ltheora -ltheoradec -o "$OUT_HTML" --preload-file $PROG_LOCATION@ -O3 -s ASYNCIFY -s SDL2_IMAGE_FORMATS='["bmp","png","xpm"]' --use-preload-plugins -s TOTAL_MEMORY=134217728 -s ALLOW_MEMORY_GROWTH=1 -s MAXIMUM_MEMORY=1073741824
+	-s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s USE_SDL_NET=2 -s USE_OGG=1 -s USE_VORBIS=1 -ltheora -ltheoradec -O3 -s ASYNCIFY -s SDL2_IMAGE_FORMATS='["bmp","png","xpm"]' --use-preload-plugins
 
 
 #	--use-port=bullet \
