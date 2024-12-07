bea293d13c50513ec409d346a30ac2cfbb856cd6
n00b <n00bskillz87@gmail.com>
2024-12-07 12:54:35 -0500

    Fixed a bullet include for windows

--- rcbasic_runtime/rc_gfx3D.h
+++ rcbasic_runtime/rc_gfx3D.h
@@ -1,7 +1,7 @@
 #ifndef RC_GFX3D_H_INCLUDED
 #define RC_GFX3D_H_INCLUDED
 
-#ifdef RC_ANDROID
+#if defined(RC_ANDROID) || defined(RC_WINDOWS)
 	#include "SDL.h"
 	#include <btBulletDynamicsCommon.h>
 	#include <BulletCollision/CollisionDispatch/btGhostObject.h>
