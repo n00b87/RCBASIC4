4b2f517de4bb7a12a012354bd109be2d1a600e55
n00b87 <n00bskillz87@gmail.com>
2025-08-13 19:20:25 -0500

    Added new functions to documentation

---
+++ doc/nirvana_docs/update_doc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=~/Programs/rcbasic3
+export RC_DOC_HOME=/home/n00b/Projects/rc_docs/RCDocs
+rcbasic_run --version
+cd /home/n00b/Projects/rc_docs/RCDocs/rc_doc_run
+rcbasic_run /home/n00b/Projects/rc_docs/RCDocs/rc_doc_run . .
