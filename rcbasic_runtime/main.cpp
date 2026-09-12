baa5ba768a21799709fc9ba9c51ca455d12cc08a
n00b87 <n00bskillz87@gmail.com>
2026-09-12 16:52:24 -0500

    Sort vertices in Triangle() by X position
    * Sort vertices by X in Triangle()
    * Doubled buffer size for floodfill()

--- rcbasic_runtime/main.cpp
+++ rcbasic_runtime/main.cpp
@@ -5189,10 +5189,13 @@ int main(int argc, char * argv[])
     //ogles2 test
     #ifdef RC_TESTING
     //rc_intern_dirChange("/home/n00b/GameDev/Ambient/Ambient_Project");
-	rc_intern_dirChange("/home/n00b/Programs/out/rcbasic/examples/Sprite_Shapes");
+	//rc_intern_dirChange("/home/n00b/Programs/rcbasic/examples/Nirvana_Platform_Game");
 	//rc_intern_dirChange("/home/n00b/Programs/out/rcbasic/examples/BSP_Map_Demo");
+	rc_intern_dirChange("/home/n00b/Programs/out/rcbasic/examples/Gold_Waves");
 	//rc_filename = "main.cbc";
 	//rc_filename = "debug.cbc";
+
+	rc_filename = "GoldWaves_Solid.cbc";
 
 	//rc_intern_dirChange("/home/n00b/Pictures/UNTITLED");
 	//rc_filename = "nv_test.cbc";
